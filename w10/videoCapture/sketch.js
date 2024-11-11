8f9560ebcf5d15bfa35b7f9f9f33bca1e5e5e1c1
gusw01 <162656220+gusw01@users.noreply.github.com>
2024-11-11 18:45:30 +0900

    10주차 수업

---
+++ w10/videoCapture/sketch.js
@@ -0,0 +1,15 @@
+let capture;
+
+function setup() {
+  createCanvas(400, 400);
+  capture = createCapture(VIDEO);
+  // console.log(capture);
+  capture.size(200, 200);
+  // console.log(capture);
+  capture.hide();
+}
+
+function draw() {
+  background(220);
+  image(capture, 0, 0, width, height);
+}
